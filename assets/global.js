b04cc57e88c7ba9dfb8065d632f678e36a36f133
mak_titi@hotmail.com <mak_titi@hotmail.com>
2021-07-29 07:08:39 +0200

    push before travel

--- assets/global.js
+++ assets/global.js
@@ -631,3 +631,54 @@ class VariantRadios extends VariantSelects {
 }
 
 customElements.define('variant-radios', VariantRadios);
+
+
+
+// vueJS
+
+// Put your application javascript here
+
+const store = Vue.reactive({
+  state: {
+    cartState: []
+  },
+
+  getCart() {
+    axios.get('/cart.js')
+      .then(response => {
+        this.state.cartState.unshift(response.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+})
+
+const miniCartState = Vue.reactive({
+  hidden: true
+})
+
+const toggleMiniCart = {
+  openCart() {
+    miniCartState.hidden = !miniCartState.hidden
+    menuState.hidden = true
+  }
+}
+
+const menuState = Vue.reactive({
+  hidden: true
+})
+
+const toggleMenu = {
+  openMenu() {
+    menuState.hidden = !menuState.hidden
+    miniCartState.hidden = true
+  }
+}
+
+const toggleClose = {
+  closeAll() {
+    miniCartState.hidden = true
+    menuState.hidden = true
+  }
+}
